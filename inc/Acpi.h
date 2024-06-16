595b9022223584317f62ddc97e2253dda56005f6
szTM06 <sztm06@gmail.com>
2024-06-17 00:41:50 +0100

    i am a perfectionist

--- inc/Acpi.h
+++ inc/Acpi.h
@@ -10,8 +10,8 @@
 #define BIOS_EXTENDED_BASE ((uint64_t)0xe0000)
 #define BIOS_EXTENDED_SIZE ((SIZE_T)((0xfffff + 1)) - BIOS_EXTENDED_BASE)
 
-int AcpiRootChecksum(PXSDP RootHdr);
-int AcpiTableChecksum(ACPISDTHeader* tableHeader);
+int               AcpiRootChecksum(PXSDP RootHdr);
+int               AcpiTableChecksum(ACPISDTHeader* tableHeader);
 
 NTSTATUS          AwiFindDirectMadt(PACPISDTHeader* Madt);
 NTSTATUS          AwiParseMadtForProcessors(PACPISDTHeader Madt);
