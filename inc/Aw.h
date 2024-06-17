b349e1061abafbbaac05bd60c89af0b3fbcd7ec5
szTM06 <sztm06@gmail.com>
2024-06-17 15:02:31 +0100

    spacing11!!1

--- inc/Aw.h
+++ inc/Aw.h
@@ -8,13 +8,13 @@
 
 //#define kprintf(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, __VA_ARGS__)
 
-#define BIOS_EXTENDED_BASE	((uint64_t)0xe0000)
-#define BIOS_EXTENDED_SIZE	((SIZE_T)((0xfffff + 1)) - BIOS_EXTENDED_BASE)
+#define BIOS_EXTENDED_BASE      ((uint64_t)0xe0000)
+#define BIOS_EXTENDED_SIZE      ((SIZE_T)((0xfffff + 1)) - BIOS_EXTENDED_BASE)
 
-#define AWI_MEMORY_START	    0x0a000
-#define AWI_MEMORY_END		    0x10000
+#define AWI_MEMORY_START        0x0a000
+#define AWI_MEMORY_END          0x10000
 
-#define AWI_ADDRESS_BASE 	    AWI_MEMORY_START
+#define AWI_ADDRESS_BASE        AWI_MEMORY_START
 #define AWI_DATA_BASE_OFFSET    0x2000
 
 typedef struct _SHARED_DATA {
@@ -41,7 +41,7 @@ NTSTATUS			AwSetupSharedData(PSHARED_DATA Data);
 
 // external imports
 
-void				AwxWriteIcr(PLAPIC, INT64, INT64);
-void				AwxReadIcr(PLAPIC, PUINT32, PUINT32);
+void                AwxWriteIcr(PLAPIC, INT64, INT64);
+void                AwxReadIcr(PLAPIC, PUINT32, PUINT32);
 
-NTSYSAPI PVOID		RtlPcToFileHeader(PVOID, PVOID*);
+NTSYSAPI PVOID      RtlPcToFileHeader(PVOID, PVOID*);
